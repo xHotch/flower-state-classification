Fix bugs in tracking and optical flow calculation

src/flower_state_classification/cv/frame_processor.py
@@ -18,8 +18,7 @@ logger=logging.getLogger(__name__)
 class FrameProcessor:
     classifier: Classifier = None
     detector: Detector = None
-    classified_plants: Dict[int, List[Plant]] = None
-    classified_plants_new: Dict[int, List[Plant]] = None
+    classified_plants_new: List[Plant] = None
     last_frame: np.ndarray = None
     
     def __init__(self, run_settings: Settings):
@@ -31,32 +30,32 @@ class FrameProcessor:
             #TODO use max frame size instead of 640x480
             self.video_writer = cv2.VideoWriter(f"{self.debug_settings.output_folder}/output.avi", cv2.VideoWriter_fourcc(*'MJPG'), 30, (640, 480))
         
-        self.image_output_folder = self.debug_settings.image_output_folder
-        self.classified_plants = {}
-        self.classified_plants_new = {}
+        self.image_output_folder = self.debug_settings.frame_output_folder
+        self.classified_plants_new = []
         self.frames_without_plants = []
-        self.optical_flow_calculator = SparseOpticalFlowCalculator(self.debug_settings)
-        self.optical_flow_calculator2 = DenseOpticalFlowCalculator(self.debug_settings)
 
+    def send_notification(self, plant: Plant):
+        notifier = self.debug_settings.notifier
+        message = f"Plant {plant.id} needs water"
+        notifier.notify(message)
 
+    def is_same_plant(self, plant: Plant, frame_nr: int, label: str, bbox):
+        if frame_nr-1 not in plant.frame_to_bounding_box:
+            return str(label) == str(plant.id)
+        
+        return str(label) == str(plant.id) or plant.frame_to_bounding_box[frame_nr-1].overlaps(bbox)
+    
     def process_frame(self, frame: np.array, frame_nr: int):
         detected_plants = None
         if self.detector:
             with Timer("Detection", logger.debug):
                 detected_plants = self.detector.predict([frame])
 
-        # Calculate optical flow on whole frame for static cameras
         optical_flow = None
-        #optical_flow_polar = None
-        if self.debug_settings.camera_mode is CameraMode.STATIC:
-            # optical_flow = self.optical_flow_calculator.calculate(frame)
-            optical_flow2 = self.optical_flow_calculator2.calculate(frame)
-                    
 
         # Classify the image using the classifier
         if detected_plants:
             for bbox, label in detected_plants:
-                optical_flow = self.optical_flow_calculator.calculate(frame, bbox)
                 plant_frame = frame.copy()
                 plant_frame = bbox.cut_frame(plant_frame)
                 logger.debug(f"Object detector detected label: {label} at location: {bbox} with confidence: {bbox.score}")
@@ -65,67 +64,74 @@ class FrameProcessor:
                 is_new_plant = True
                 current_plant = None
 
-                if self.classified_plants_new and frame_nr-1 in self.classified_plants_new:
-                    for plant in self.classified_plants_new[frame_nr-1]:
-                        if label == plant.id or plant.frame_to_bounding_box[frame_nr-1].overlaps(bbox):
+                if self.classified_plants_new:
+                    for plant in self.classified_plants_new:
+                        if self.is_same_plant(plant,frame_nr,label, bbox):
                             plant.frame_to_bounding_box[frame_nr] = bbox
-                            if frame_nr not in self.classified_plants_new:
-                                self.classified_plants_new[frame_nr] = []
-                            self.classified_plants_new[frame_nr].append(plant)
                             current_plant = plant
                             is_new_plant = False
                             break
 
                 if is_new_plant:
-                    optical_flow_calculator = SparseOpticalFlowCalculator(self.debug_settings) if self.debug_settings.camera_mode is CameraMode.MOVING else self.optical_flow_calculator
+                    optical_flow_calculator = SparseOpticalFlowCalculator(self.debug_settings, label) # if self.debug_settings.camera_mode # is CameraMode.MOVING else self.optical_flow_calculator
                     current_plant = Plant(label, {}, True, None, None, optical_flow_calculator)
                     current_plant.frame_to_bounding_box[frame_nr] = bbox
-                    if frame_nr not in self.classified_plants_new:
-                        self.classified_plants_new[frame_nr] = []
-                    self.classified_plants_new[frame_nr].append(current_plant)
+                    self.classified_plants_new.append(current_plant)
+                    if self.debug_settings.write_plant_images:
+                        cv2.imwrite(self.debug_settings.plant_output_folder + f"/{label}_frame-{frame_nr}.jpg", cv2.cvtColor(plant_frame, cv2.COLOR_RGB2BGR))
 
                 # Calculate Optical Flow
-                green_mask = None
-                if self.last_frame is not None:
-                    if self.debug_settings.camera_mode is CameraMode.MOVING:
-                        optical_flow = current_plant.optical_flow_calculator.calculate(plant_frame)
-                    elif self.debug_settings.camera_mode is CameraMode.STATIC:
-                        needs_water = current_plant.optical_flow_calculator.classify(optical_flow)
-                        self.optical_flow_calculator2.classify(optical_flow2)
+                with Timer("Optical Flow"):
+                    green_mask = None
+                    if self.last_frame is not None:
+                        if self.debug_settings.camera_mode is CameraMode.MOVING:
+                            optical_flow = current_plant.optical_flow_calculator.calculate(plant_frame)
+                        elif self.debug_settings.camera_mode is CameraMode.STATIC:
+                            optical_flow = current_plant.optical_flow_calculator.calculate(frame, bbox)
+                            needs_water = current_plant.optical_flow_calculator.classify(optical_flow)
+                            if needs_water:
+                                current_plant.is_healthy = False
+                                current_plant.unhealthy_frames.append(frame_nr)
+                                self.send_notification(current_plant)
+                                if self.debug_settings.write_plant_images and current_plant.unhealthy_frames[0] == frame_nr:
+                                    filename = f"{self.debug_settings.plant_output_folder}/{label}_frame-{frame_nr}_needswater.jpg"
+                                    cv2.imwrite(filename, cv2.cvtColor(plant_frame, cv2.COLOR_RGB2BGR))
 
 
-                
-                classifier_label = self.classifier.predict(plant_frame) if self.classifier else "No classifier"
-                gc.collect()
-                logger.debug(f"Classifier detected label: {classifier_label}")
+
+                with Timer("Classification", logger.debug):
+                    classifier_label = self.classifier.predict(plant_frame) if self.classifier else "No classifier"
+                    # gc.collect()
+                    logger.debug(f"Classifier detected label: {classifier_label}")
 
                 if self.debug_settings.show_plant_frames:
                     cv2.putText(plant_frame, f"{label}, {classifier_label}, {bbox.score}", (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                    cv2.imshow("plant_frame", cv2.cvtColor(plant_frame, cv2.COLOR_RGB2BGR))
+                    cv2.imshow(f"plant_frame {label}", cv2.cvtColor(plant_frame, cv2.COLOR_RGB2BGR))
                     cv2.waitKey(1)
 
                 if self.debug_settings.show_optical_flow and optical_flow is not None:
                     output_frame = frame.copy()
-                    # optical_arrows = self.optical_flow_calculator.visualize(output_frame, optical_flow)
-                    # cv2.imshow("optical_flow_arrows", cv2.cvtColor(optical_arrows, cv2.COLOR_RGB2BGR))
-                    #cv2.waitKey(-1)
+                    optical_arrows = current_plant.optical_flow_calculator.visualize(output_frame)
+                    cv2.imshow("optical_flow_arrows", cv2.cvtColor(optical_arrows, cv2.COLOR_RGB2BGR))
+                    cv2.waitKey(1)
 
                 if self.debug_settings.plot_optical_flow and optical_flow is not None:
                     if frame_nr % 10 == 0:
-                        current_plant.optical_flow_calculator.plot(current_plant.optical_flow_calculator.flow_angles, current_plant.optical_flow_calculator.flow_magnitudes, id = current_plant.id, save = True)
-                        current_plant.optical_flow_calculator.plot(current_plant.optical_flow_calculator.total_angles, current_plant.optical_flow_calculator.total_magnitudes, id = str(current_plant.id) + "_total_flow", save = True)
+                        current_plant.optical_flow_calculator.plot(id = current_plant.id, save = True)
 
         else:
             logger.debug(f"Object detector did not detect any plants in frame {frame_nr}")
             self.frames_without_plants.append(frame_nr)
 
         output_frame = frame.copy()
-        if self.debug_settings.show_bboxes and frame_nr in self.classified_plants_new:
-            for plant in self.classified_plants_new[frame_nr]:
-                box_label(output_frame, plant.frame_to_bounding_box[frame_nr], f"{plant.id}, {plant.classifier_label}")
+        if self.debug_settings.show_bboxes:
+            for plant in self.classified_plants_new:
+                if frame_nr in plant.frame_to_bounding_box:
+                    box_label(output_frame, plant.frame_to_bounding_box[frame_nr], f"{plant.id}, {plant.classifier_label}")
         else:
             logger.debug(f"No bounding boxes to show in frame {frame_nr}")
 
+        output_frame = cv2.putText(output_frame, f"Frame {frame_nr}", (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         # Displaying the image if the debug setting is set
         if self.debug_settings.show_frame:
             cv2.imshow("frame", cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR))
@@ -135,7 +141,7 @@ class FrameProcessor:
         if self.debug_settings.write_video:
             self.video_writer.write(cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR))
         
-        if self.debug_settings.write_images:
+        if self.debug_settings.write_frames:
             output_filename = os.path.join(self.image_output_folder, f"frame_{frame_nr}.jpg")
             cv2.imwrite(output_filename, cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR))
         

src/flower_state_classification/cv/models/ultralytics_models/ultralytics_models.py
@@ -16,10 +16,13 @@ class UltralyticsDetector(Detector):
             self.model = RTDETR(os.path.join(self.dir,model_name))
         else:
             raise ValueError(f"Unknown model name: {model_name}")
+        self.reset_tracker = True
     
     def predict(self, frames: List[np.array]) -> List[Tuple[BoundingBox, str]]:
         for frame in frames:
-            result = self.model.track(source=frames, conf=self.threshold, persist=True, verbose=False)
+            persist = not self.reset_tracker
+            result = self.model.track(source=frames, conf=self.threshold, persist=persist, verbose=False)
+            self.reset_tracker = False
             names = result[0].names
             boxes = result[0].boxes.cpu().numpy()
             #return [(BoundingBox.from_relative(boxes.xyxy[i], boxes.conf[i]),names[int(boxes.cls[i])]) for i in range(len(boxes))]

src/flower_state_classification/cv/optical_flow.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 import copy
 import os
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 import cv2
 from matplotlib import pyplot as plt
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('agg')
 plt.rcParams['animation.html'] = 'jshtml'
 import numpy as np
 import logging
@@ -22,27 +22,33 @@ class OpticalFlowCalculator(ABC):
     green_threshold_high: np.array = np.array([90, 255, 255]) 
 
     # Debug variables
-    flow_angles = []
-    flow_magnitudes = []
+    flow_angles: List[np.ndarray]
+    flow_magnitudes: List[np.ndarray]
 
-    total_angles = []
-    total_magnitudes = []
+    total_angles: List[np.ndarray]
+    total_magnitudes: List[np.ndarray]
 
-    needs_water = False
+    status: List
     previously_notified = False
 
-    def __init__(self, debug_settings: Settings) -> None:
+    def __init__(self, debug_settings: Settings, plant_id = "") -> None:
+        logger.warning("Optical flow calculator initialized")
+        self.flow_angles = []
+        self.flow_magnitudes = []
+        self.total_angles = []
+        self.total_magnitudes = []
         self.debug_settings = debug_settings
-        self.fig, (self.axs, self.axs_mag) = plt.subplots(2, sharex=True)
-        self.fig.suptitle("Optical flow over time")
-
+        
+        self.status = []
+        self.plant_id = plant_id
+        self.scaled_threshold = None
         # self.fig.show()
 
     def get_green_mask(self, frame: np.ndarray, bbox: BoundingBox = None) -> np.ndarray:
         hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
         mask = cv2.inRange(hsv, self.green_threshold_low, self.green_threshold_high)
         if bbox:
-            bbox.mask_frame(mask, value = 0)
+            mask = bbox.mask_frame(mask, value = 0)
         
         if self.debug_settings.show_green_mask:
             if self.debug_settings.show_green_mask and mask is not None:
@@ -66,9 +72,9 @@ class OpticalFlowCalculator(ABC):
 
     def _calculate_angle_and_magnitude(self, optical_flow: np.ndarray, mask: np.ndarray = None) -> Tuple[np.ndarray, np.ndarray]:
         if optical_flow is None:
-            return
+            return None, None
         
-        if mask is not None:
+        if mask is not None: #TODO: check if this is correct
             optical_flow = cv2.bitwise_and(optical_flow, optical_flow, mask=mask)
 
         if len(optical_flow.shape) == 2 or optical_flow.shape[2] == 2:
@@ -83,7 +89,7 @@ class OpticalFlowCalculator(ABC):
 
         if not angle.any() or not magnitude.any():
             logger.debug("No optical flow detected")
-            return
+            return None, None
 
         average_angle = np.mean(angle)
         median_angle = np.median(angle)
@@ -91,13 +97,12 @@ class OpticalFlowCalculator(ABC):
         average_magnitude = np.mean(magnitude)
         median_magnitude = np.median(magnitude)
 
-        logger.info("Optical flow angle: average: {}, median: {}".format(average_angle, median_angle))
-        logger.info("Optical flow magnitude: average: {}, median: {}".format(average_magnitude, median_magnitude))
+        logger.debug("Plant: {}, Optical flow angle: average: {}, median: {}".format(self.plant_id, average_angle, median_angle))
+        logger.debug("Optical flow magnitude: average: {}, median: {}".format(average_magnitude, median_magnitude))
 
         return angle, magnitude
     
-    def plot(self, angles, magnitudes, id: str = None, save = False):
-        
+    def _plot_angle_and_magnitude(self, angles: np.ndarray, magnitudes: np.ndarray) -> None:
         average_angle = [np.mean(angle) for angle in angles]
         average_magnitude = [np.mean(magnitude) for magnitude in magnitudes]
 
@@ -107,34 +112,65 @@ class OpticalFlowCalculator(ABC):
 
         #axs_mag.ylabel("Magnitude [px]")
         self.axs_mag.plot(average_magnitude)
+
+    def plot(self, id: str = None, save = False):
+        self.fig, (self.axs, self.axs_mag) = plt.subplots(2, sharex=True)
+        self.fig.suptitle("Optical flow over time")
+
+        self._plot_angle_and_magnitude(self.flow_angles, self.flow_magnitudes)
+        self._plot_angle_and_magnitude(self.total_angles, self.total_magnitudes)
         
         minimum_angle = self.debug_settings.minimum_angle
         maximum_angle = self.debug_settings.maximum_angle
 
-        self.axs.axhline(y=minimum_angle, color='g', linestyle='--', label="Minimum angle", linewidth=2)
-        self.axs.axhline(y=maximum_angle, color='g', linestyle='--', label="Maximum angle", linewidth=2)
+        self.axs.axhline(y=minimum_angle, color='g', linestyle='--', label="Minimum angle", linewidth=0.5)
+        self.axs.axhline(y=maximum_angle, color='g', linestyle='--', label="Maximum angle", linewidth=0.5)
         
+        self.axs_mag.axhline(y=self.debug_settings.magnitude_threshold, color='g', linestyle='--', label="Magnitude threshold", linewidth=2)
+        self.axs_mag.axhline(y=self.scaled_threshold, color='r', linestyle='--', label="Scaled threshold", linewidth=2)
 
-        if self.needs_water and not self.previously_notified:
-            self.axs.axvline(x=len(angles), color='r', linestyle='--')
-            self.axs_mag.axvline(x=len(angles), color='r', linestyle='--')
-            self.previously_notified = True
 
+        start_idx = None
+        for (index, status) in self.status:
+            if status == "needs_watering" and start_idx is None:
+                start_idx = index
+            elif not status == "needs_watering" and start_idx is not None:
+                end_idx = index
+                
+                self.axs.axvspan(start_idx, end_idx, alpha=0.2, color='blue')
+                self.axs_mag.axvspan(start_idx, end_idx, alpha=0.2, color='blue')
+
+                #self.axs.fill_between(x_values[start_idx:end_idx], 0, 1, alpha=0.3, color='blue')
+                #self.axs_mag.fill_between(x_values[start_idx:end_idx], 0, 1, alpha=0.3, color='blue')
+
+                start_idx = None
+
+        if start_idx is not None and self.status[-1][1] == "needs_watering":
+            end_idx = len(self.status)
+            self.axs.axvspan(start_idx, end_idx, alpha=0.2, color='blue')
+            self.axs_mag.axvspan(start_idx, end_idx, alpha=0.2, color='blue')
+
+
+        self.axs.legend(["Average", "Accumulated"])
+        self.axs_mag.legend(["Average", "Accumulated"])
+        
         if save:
             folder = self.debug_settings.get_output_folder("optical_flow")
             self.fig.savefig(os.path.join(folder,f"{id}.png"))
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
-
+        plt.close(self.fig)
+        
 
 # https://docs.opencv.org/4.7.0/d4/dee/tutorial_optical_flow.html
 class SparseOpticalFlowCalculator(OpticalFlowCalculator):
     
-    last_points: List[Tuple] = []
+    last_points: List[Tuple]
 
-    def __init__(self, settings = None) -> None:
-        super().__init__(settings)
+    def __init__(self, settings = None, plant_id = "") -> None:
+        super().__init__(settings, plant_id)
         self.color = np.random.randint(0, 255, (100, 3))
+        self.last_points = []
         ...
 
     # Parameters for Shi-Tomasi corner detection
@@ -166,14 +202,17 @@ class SparseOpticalFlowCalculator(OpticalFlowCalculator):
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         return frame_gray
     
-    def visualize(self, image: np.ndarray, old_keypoints, new_keypoints) -> np.ndarray:
+    def visualize(self, image: np.ndarray) -> np.ndarray:
+        old_keypoints, new_keypoints = self.last_points[0][0], self.last_points[-1][0]
+        
         # Create a mask image for drawing purposes
         output = np.zeros_like(image)
         frame = image.copy()
         im2 = image.copy()
         for i, (new, old) in enumerate(zip(new_keypoints, old_keypoints)):
-            a, b = new.ravel()
-            c, d = old.ravel()
+            a, b = old.ravel()
+            c, d = new.ravel()
+            
             
             im2 = cv2.arrowedLine(im2, (int(a), int(b)), (int(c), int(d)), self.color[i].tolist(), 2)
             output = cv2.line(output, (int(a), int(b)), (int(c), int(d)), self.color[i].tolist(), 2)
@@ -195,6 +234,7 @@ class SparseOpticalFlowCalculator(OpticalFlowCalculator):
 
         if not self.last_points:
             p0 = cv2.goodFeaturesToTrack(frame1_gray, mask = mask, **self.feature_params)
+            logger.warning("Found {} corners for plant {}".format(len(p0), self.plant_id))
         else:
             p0 = self.last_points[-1][0]
             # p0 = cv2.goodFeaturesToTrack(frame1_gray, mask = mask, **self.feature_params)
@@ -213,30 +253,41 @@ class SparseOpticalFlowCalculator(OpticalFlowCalculator):
 
     def classify(self, optical_flow: np.ndarray) -> bool:
         angle, magnitude = self._calculate_angle_and_magnitude(optical_flow, None)
+
+        if angle is None or magnitude is None:
+            logger.warning("Could not calculate angle or magnitude for plant {}".format(self.plant_id))
+            return False
+        
         self.flow_angles.append(angle)
         self.flow_magnitudes.append(magnitude)
         
+        index = len(self.flow_angles)
         if len(self.last_points) < 2:
+            self.status.append((index, "enough_water"))
             return False
         total_flow = self.last_points[-1][0] - self.last_points[0][0]
         total_angle, total_magnitude = self._calculate_angle_and_magnitude(total_flow, None)
+
         self.total_angles.append(total_angle)
         self.total_magnitudes.append(total_magnitude)
 
         total_angle = np.mean(total_angle)
         total_magnitude = np.mean(total_magnitude)
 
-        if total_angle > self.debug_settings.minimum_angle and total_angle < self.debug_settings.maximum_angle:
-            logger.info("Optical flow classified as downwards")
+        self.scaled_threshold = self.debug_settings.magnitude_threshold_scaled * self.last_bbox.height() if not self.scaled_threshold else self.scaled_threshold
 
-            if total_magnitude > self.debug_settings.magnitued_threshold:
-                logger.info("Optical flow classified as downwards and strong")
-                #TODO notifiy
-                self.needs_water = True
+        if total_angle > self.debug_settings.minimum_angle and total_angle < self.debug_settings.maximum_angle:
+            logger.debug("Optical flow classified as downwards")
+
+            if total_magnitude > self.debug_settings.magnitude_threshold:
+                logger.debug("Optical flow classified as downwards and strong")
+                
+                self.status.append((index, "needs_watering"))
                 return True
             else:
-                logger.info("Optical flow classified as downwards but not enough magnitude")
+                logger.debug("Optical flow classified as downwards but not enough magnitude")
 
+        self.status.append((index, "enough_water"))
         return False
 
 class DenseOpticalFlowCalculator(OpticalFlowCalculator):